57a8754ae4cf4cde7675e55fc16f6dd91c34b29f
Alexey Sosov <alexey.sosov@zyfra.com>
2023-06-10 21:54:28 +0300

    rework import_tool
    Вернул try except блок на всю функцию

--- func_call_patcher/import_tool.py
+++ func_call_patcher/import_tool.py
@@ -9,15 +9,12 @@ class ImportTool:
         raise exceptions.ItCannotBeImported(f"это не может быть импортировано {path_to_func}")
 
     @classmethod
-    def import_class_and_method_from_string(cls, path_to_func: str) -> Tuple[Type, Callable]:
+    def _import_class_and_method_from_string(cls, path_to_func: str) -> Tuple[Type, Callable]:
         path_to_method_splitted = path_to_func.split('.')
         full_path_to_module = '.'.join(path_to_method_splitted[:-3])
         module_name, class_name, method_name = path_to_method_splitted[-3:]
 
-        try:
-            exec(f'from {full_path_to_module} import {module_name}')
-        except Exception:
-            cls._raise_it_cannot_be_imported_error(path_to_func=path_to_func)
+        exec(f'from {full_path_to_module} import {module_name}')
         module_name = locals()[module_name]
         class_obj = getattr(module_name, class_name)
         method = getattr(class_obj, method_name)
@@ -25,7 +22,14 @@ class ImportTool:
         return class_obj, method
 
     @classmethod
-    def import_func_from_string(cls, path_to_func: str) -> Callable:
+    def import_class_and_method_from_string(cls, path_to_func: str) -> Tuple[Type, Callable]:
+        try:
+            return cls._import_class_and_method_from_string(path_to_func=path_to_func)
+        except Exception:
+            cls._raise_it_cannot_be_imported_error(path_to_func=path_to_func)
+
+    @classmethod
+    def _import_func_from_string(cls, path_to_func: str) -> Callable:
         path_to_func_splitted = path_to_func.split('.')
         full_path_to_module = '.'.join(path_to_func_splitted[:-2])
         module_name, func_name = path_to_func_splitted[-2:]
@@ -39,3 +43,10 @@ class ImportTool:
         func = getattr(module, func_name)
 
         return func
+
+    @classmethod
+    def import_func_from_string(cls, path_to_func: str) -> Callable:
+        try:
+            return cls._import_func_from_string(path_to_func=path_to_func)
+        except Exception:
+            cls._raise_it_cannot_be_imported_error(path_to_func=path_to_func)
